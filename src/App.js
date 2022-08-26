623c6c50168e8e47edc26d65994f549154cc98f8
Bautista <bautista.rostand@gmail.com>
2022-08-25 21:51:56 -0300

    prueba

--- src/App.js
+++ src/App.js
@@ -1,7 +1,4 @@
-
 import './App.css';
-import Footer from './components/Footer';
-import NavBar from './components/NavBar';
 
 function App() {
   let imagen = "kqXt7Sq2.png";
@@ -10,22 +7,13 @@ function App() {
 let descripcion = "Imaginá la sensación del clásico Cuarto de Libra.Imaginalo con un medallón de deliciosa carne 100% vacuna, queso cheddar, cebolla, kétchup y mostaza ¿Listo? Ahora duplicá esa sensación. Ya tenés el Doble Cuarto en la cabeza."
 const producto = {id: 1, image:imagen, title:titulo, calories:calorias, description:descripcion}; 
 
-const productos = [
-  {id:1, nombre:"Cuarto de Libra", precio:"980"},
-  {id:2, nombre:"Triple Bacon", precio:"1230"},
-  {id:3, nombre:"Doble Bacon", precio:"1120"},
-  {id:4, nombre:"Macnifica", precio:"1400"}
-];
+
   return (
-    <div className="container">
-      <NavBar />
-      <h3>Productos</h3>
+    <div className="container-fluid">
+      
+
       <ul>
-      {
-        productos.map(item =>(
-          <li key={item.id}>{item.nombre} - ${item.precio}</li>
-        ))
-      }
+
       </ul>
       <div className="row">
         <div className="col-md-4 offset-md-2">
@@ -37,7 +25,7 @@ const productos = [
           <p>{producto.description}</p>
           </div>
         </div>
-        <Footer /> 
+       
     </div>
   );
 }
