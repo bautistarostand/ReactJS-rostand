5352ebe511c173c54651ce20f32e10d7408b6b40
Bautista <bautista.rostand@gmail.com>
2022-08-31 23:07:10 -0300

    itemlist

--- src/App.js
+++ src/App.js
@@ -1,18 +1,27 @@
 import './App.css';
 import Footer from './Assets/Footer';
+import ItemListContainer from './Assets/itemListContainer';
 import NavBar from './Assets/NavBar';
+import Titulo from './Assets/Titulo';
 
 function App() {
+  const lista = ["Cuarto de libra", "Big Mac", "McNifica"];
+  const items = lista.map (items => (
+    <ItemListContainer valor={items} />
+  ));
   let imagen = "kqXt7Sq2.png";
   let titulo = "Doble Cuarto de Libra con Queso";
   let calorias = "771 kcal";
 let descripcion = "Imaginá la sensación del clásico Cuarto de Libra.Imaginalo con un medallón de deliciosa carne 100% vacuna, queso cheddar, cebolla, kétchup y mostaza ¿Listo? Ahora duplicá esa sensación. Ya tenés el Doble Cuarto en la cabeza."
 const producto = {id: 1, image:imagen, title:titulo, calories:calorias, description:descripcion}; 
 
-
   return (
     <div className="container-fluid fondo_amarillo">
       <NavBar />
+      <Titulo valor="Nuestras Hamburguesas" />
+      <ul>
+        {items}
+      </ul>
       <hr />
       <div className="row">
         <div className="col-md-4 offset-md-2">
@@ -31,4 +40,3 @@ const producto = {id: 1, image:imagen, title:titulo, calories:calorias, descript
 }
 
 export default App;
-
---
+++ src/Assets/itemListContainer.jsx
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+
+class ItemListContainer extends Component{
+
+    render() {
+        return(
+            <li>
+                <a href="#">{this.props.valor}</a>
+            </li>
+        )
+    }
+
+}
+
+export default ItemListContainer;
